cc7e7a4b61d6f1ff821f94e81a414a51295fcf28
Anushka Bahuguna <anushkabahugunaa@gmail.com>
2021-01-11 20:28:47 +0530

    Created console app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Local Hack Day: Build");
+            var name = "Anushka";
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
